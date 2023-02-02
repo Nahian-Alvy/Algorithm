796cfb4dd3ce7d7f1ce351eb2e482d2259d7505e
Nahian-Alvy <117886480+Nahian-Alvy@users.noreply.github.com>
2023-02-02 22:40:02 +0600

    Update GCD.cpp

--- GCD.cpp
+++ GCD.cpp
@@ -1,3 +1,19 @@
+/*Euclid’s algorithm
+1: procedure Euclid(a, b) ▷ The g.c.d. of a and b
+2: r ← a mod b
+3: while r ̸ = 0 do ▷ We have the answer if r is 0
+4: a ← b
+5: b ← r
+6: r ← a mod b
+7: end while
+8: return b ▷ The gcd is b
+9: end procedure */
+
+
+
+
+
+
 #include <iostream>
 
 using namespace std;
