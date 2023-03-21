d6fed756abd85e4e4bca4fcbf088b23fa4f56b78
Nahian-Alvy <117886480+Nahian-Alvy@users.noreply.github.com>
2023-03-21 09:30:44 +0600

    Create graph.cpp

---
+++ graph.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+#define n 100 
+
+int main()
+{
+    int g[n][n];
+    int nodes, edges;
+    cin>>nodes>>edges;
+    int v,u;
+    for(int i=0;i<edges;i++)
+    {
+        cin>>u>>v;
+        g[u][v]=1;
+        g[v][u]=1;
+    }
+for (int i=0;i<nodes;i++)
+{
+    for (int j=0;j<nodes;j++)
+    {
+        cout<<g[i][j]<<" ";
+    }
+    cout<<endl;
+}
+    return 0;
+}
